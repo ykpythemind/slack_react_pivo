dbd38c4c361e9500b7e0dbacf4b28cb32c11743c
Yukito Ito <yukibukiyou@gmail.com>
2019-08-17 00:19:13 +0900

    ya

--- entry.rb
+++ entry.rb
@@ -1,5 +1,39 @@
-require 'dotenv/load'
+require 'bundler/setup'
+
+require 'dotenv'
+require 'slack-ruby-client'
 require 'sinatra'
+require 'tracker_api'
+
+Dotenv.load
+
+Slack.configure do |config|
+  config.token = ENV['SLACK_API_TOKEN']
+end
+
+$client = Slack::Web::Client.new
+
+pivo_client = TrackerApi::Client.new(token: ENV['PIVOTAL_API_TOKEN'])
+$project = pivo_client.project(ENV['PIVOTAL_PROJECT_ID'])
+
+class ReactionAdded
+  def self.call(event_data)
+    reaction = event_data["reaction"]
+    puts reaction
+
+    channel = event_data.dig("item", "channel")
+    ts = event_data.dig("item", "ts")
+    return if channel.nil? || ts.nil?
+
+    res = $client.channels_history(channel: channel)
+    message = res.messages.select { |message| message.ts == ts }[0]
+    return if message.blank?
+
+    text = message.text
+    puts text
+    $project.create_story(name: text.slice(0, 40), description: text)
+  end
+end
 
 get '/' do
   "Hello World! #{ENV["TEST"]}"
@@ -19,15 +53,14 @@ post '/events' do
     team_id = request_data['team_id']
     event_data = request_data['event']
 
-    # Events have a "type" attribute included in their payload, allowing you to handle different
-    # event payloads as needed.
     case event_data['type']
-    when 'team_join'
+    when 'reaction_added'
       # Event handler for when a user joins a team
+      ReactionAdded.call(event_data)
     else
       # In the event we receive an event we didn't expect, we'll log it and move on.
-      puts "Unexpected event:\n"
-      puts JSON.pretty_generate(request_data)
+      # puts "Unexpected event:\n"
+      # puts JSON.pretty_generate(request_data)
     end
     # Return HTTP status code 200 so Slack knows we've received the event
     status 200
